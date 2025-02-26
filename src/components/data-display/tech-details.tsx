076bee1f46e7388079bf90d2734e04452405fa67
Ankit Sharma <akkumar150277572@gmail.com>
2025-02-26 23:26:29 +0530

    changes

--- src/components/data-display/tech-details.tsx
+++ src/components/data-display/tech-details.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { TechDetails } from '@/lib/types';
 import Typography from '@/components/general/typography';
 import Link from '@/components/navigation/link';
 import ImageWrapper from '@/components/data-display/image-wrapper';
+import type { TechDetails } from '@/lib/types';
 
 const TechDetails = ({ url, logo, darkModeLogo, label }: TechDetails) => {
   return (
